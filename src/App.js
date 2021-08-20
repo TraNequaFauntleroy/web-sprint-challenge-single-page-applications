4302fc7b5755cd3e39c50d8f78b8ed733793c5f7
NeNeTheCreative <shecodes@nenethecreative.com>
2021-08-20 10:05:55 -0400

    homepage built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { Route, Link } from "react-router-dom";
+import Home from './components/Home'
+import PizzaForm from './components/Pizza-Form'
+import axios from 'axios';
 
 const App = () => {
+  const [pizza, setPizza] = useState([])
+
+  useEffect(() => {
+    axios.get('https://reqres.in/api/orders')
+      .then(res => {
+        setPizza(res.data)
+      })
+  }, [])
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <nav>
+        <h1 className='home-header'>Let's Make A Pizza</h1>
+        <div className='nav-links'>
+          <Link to='/'>Home</Link>
+          <Link to='/pizza'>Order Pizza</Link>
+        </div>
+      </nav>
+      <Route path={'/pizza/'}>
+        <PizzaForm />
+      </Route>
+      <Route exact path ='/'>
+        <Home />
+      </Route>
+  
+    </div>
   );
 };
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import react from 'react';
+
+
+export default function Home() {
+    const routeToOrder = () => {
+
+    }
+
+    return (
+        <div className='home-wrapper'>
+            <img
+                className='home-imaage'
+                src='https://images.unsplash.com/photo-1537734796389-e1fc293cf856?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBpenphfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60'
+                alt='man holding a ball of pizza dough'
+            />
+            <button
+                onClick={routeToOrder}
+                className='md-button order-button' 
+
+            >Order Now</button>
+        </div>
+    )
+
+}
+
